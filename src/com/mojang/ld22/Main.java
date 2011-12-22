f4e9f9dac51658dea97b7dc6f5ed1905e0779120
Anger Jérémy <kidanger@gmail.com>
2011-12-22 11:53:24 +0100

    Add save/loading, better respawn, fix crash on resize

---
+++ src/com/mojang/ld22/Main.java
@@ -0,0 +1,15 @@
+package com.mojang.ld22;
+
+import com.mojang.ld22.*;
+import com.mojang.ld22.gfx.SpriteSheet;
+import com.mojang.ld22.level.Level;
+import com.mojang.ld22.level.tile.Tile;
+import com.mojang.ld22.screen.DeadMenu;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.start();
+	}
+}
